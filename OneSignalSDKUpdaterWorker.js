b3f0ad570efdaca0a4950c53174c8dc11ea1af6a
Sarita4553 <saritapathania9@gmail.com>
2025-06-30 23:10:07 +0530

    Update OneSignalSDKUpdaterWorker.js

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,1 +1,6 @@
-importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+'use strict';
+
+// This file is OneSignal's updater service worker
+// https://github.com/OneSignal/OneSignal-Website-SDK/blob/main/worker-updater.js
+
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKUpdaterWorker.js');
