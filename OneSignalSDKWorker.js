708c32372c81ce21710ef82db2fa89eb60c88aa5
Sarita4553 <saritapathania9@gmail.com>
2025-06-30 23:09:25 +0530

    Create OneSignalSDKWorker.js

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// This file is OneSignal's service worker
+// https://github.com/OneSignal/OneSignal-Website-SDK/blob/main/worker.js
+
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
